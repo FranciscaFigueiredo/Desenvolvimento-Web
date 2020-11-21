2f713c3169c00afe306cd403ba6174959f7b4977
FranciscaFigueiredo <48726063+FranciscaFigueiredo@users.noreply.github.com>
2020-11-21 01:37:08 -0300

    Rotas e Parâmetros

--- nodeJs/server.js
+++ nodeJs/server.js
@@ -1,16 +1,36 @@
 const express = require(" express ")
 
 const app = express()
+app.use(express.json())
+
+/*
+ * GET: buscar ou listar uma informação no backend
+ * POST: criar uma informação no backend
+ * PUT: alterar uma informação no backend
+ * DELETE: deletar uma informação no backend 
+ */
+
+/*
+ * Parâmetros
+ * Query: recebe os parâmetros enviados na rota
+ * Route params: parâmetros utilizados para identificar um recurso
+ * Request Body: corpo da requisição
+ */
 
 app.get('/users', (req, res) => {
-    app.json({
+    const params = req.query
 
-        nome: "Francisca Figueiredo",
-        empresa: 'UESB'
-        
-    })
+    res.json(params)
 })
 
+app.post('/users/:id', (req, res) => {
+    const params = req.params
+    res.json(params)
+})
+
+app.post('/users', (req, res) => {
+    const params = req.body
+    res.json(params)
 })
 
 app.listener(3001)
